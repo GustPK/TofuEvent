4262c8cf44fca879cb6579033e10c44d62bed667
GustPK <pasawat.k@ku.th>
2023-08-08 16:07:06 +0700

    logincontrolleredit

--- src/main/java/cs211/project/controllers/login/LoginController.java
+++ src/main/java/cs211/project/controllers/login/LoginController.java
@@ -1,10 +1,13 @@
 package cs211.project.controllers.login;
 
+import cs211.project.services.FXRouter;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -19,6 +22,16 @@ public class LoginController {
     private Hyperlink ForgetPassword;
     @FXML
     private Hyperlink Developers;
+
+    @FXML
+    private void onDevelopersHyperlinkClick() throws IOException{
+        FXRouter.goTo("developer");
+    }
+
+    @FXML
+    private void onCreateAccountHyperlinkClick() throws IOException{
+        FXRouter.goTo("register");
+    }
 
 
 }
