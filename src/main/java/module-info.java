69028630bf94a72d01d2b2b78eae2546efbe297b
DEASII <jirametharat@gmail.com>
2023-08-09 14:41:31 +0700

    add BackButton

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,7 @@ module cs211.project.cs211661project {
 
     exports cs211.project.controllers.login;
     opens cs211.project.controllers.login to javafx.fxml;
+
     exports cs211.project.controllers.main;
     opens cs211.project.controllers.main to javafx.fxml;
 }
