dea6a5df0ef09425d056133794010a0965784825
DEASII <jirametharat@gmail.com>
2023-10-15 03:24:15 +0700

    my team view

--- src/main/java/cs211/project/controllers/team/TeamItemController.java
+++ src/main/java/cs211/project/controllers/team/TeamItemController.java
@@ -1,12 +1,20 @@
 package cs211.project.controllers.team;
 
+import cs211.project.models.account.Account;
+import cs211.project.models.collections.EventList;
 import cs211.project.models.event.Team;
 import cs211.project.models.event.Event;
+import cs211.project.services.AccountListDatasource;
+import cs211.project.services.Datasource;
+import cs211.project.services.EventListDatasource;
 import cs211.project.services.FXRouter;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.ImagePattern;
 
+import java.io.File;
 import java.io.IOException;
 
 public class TeamItemController {
@@ -18,11 +26,24 @@ public class TeamItemController {
     private Label teamName;
     @FXML
     private ImageView eventImg;
-    private Event event;
+    private Event eventTamp;
     private Team team;
+
+    private Datasource<EventList> eventListDatasource;
+    private EventList eventList;
 
 
     public void setData(Team team){
+        eventListDatasource = new EventListDatasource();
+        eventList = eventListDatasource.readData();
+        for(Event event : eventList.getEvents())
+            if (event.getName().equals(team.getEventName()))
+                eventTamp = event;
+
+        String imagePath = "data/images/" + eventTamp.getImgEvent();
+        File imageFile = new File(imagePath);
+        Image profileImage = new Image(imageFile.toURI().toString());
+        eventImg.setImage(profileImage);
         this.team = team;
         eventNameLabel.setText(team.getEventName());
         teamName.setText(team.getTeamName());
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -24,8 +24,7 @@ module cs211.project.cs211661project {
     exports cs211.project.models.account;
     opens cs211.project.models.account to javafx.fxml;
 
-    exports cs211.project.models;
-    opens cs211.project.models to javafx.fxml;
+
 
     exports cs211.project.models.collections;
     opens cs211.project.models.collections to javafx.fxml;
